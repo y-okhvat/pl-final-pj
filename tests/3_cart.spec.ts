8bcaf82b148b15c782caec241b29b5f1267b35f0
y-okhvat <lizaokhvat14031999@gmail.com>
2025-05-05 20:24:38 +0200

    change test for page object

--- tests/3_cart.spec.ts
+++ tests/3_cart.spec.ts
@@ -1,26 +1,27 @@
 import { test, expect } from "@playwright/test";
+import { productPage } from '../pages/ProductPage';
 
 test('Login', async ({page}) => {
-    const productLocator = 'text=Slip Joint Pliers';
-    const cartLocator = '[data-test="add-to-cart"]';
-   const cartIconLocator = '[data-test="nav-cart"]';
+    const cartLocator = new productPage(page) ;
 
 
     await page.goto('https://practicesoftwaretesting.com');
-    await page.locator(productLocator).click();
+    await cartLocator.productLocator.click();  
 
-    await expect(page).toHaveURL('https://practicesoftwaretesting.com/product/01JRA36BZPT9NPYG3F8TF461J1');
+    
     await expect(page.locator('[data-test="product-name"]')).toContainText('Slip Joint Pliers');
     await expect(page.locator('[data-test="unit-price"]')).toContainText('9.17');
 
-    await page.locator(cartLocator).click();
-    await expect(page.locator('[aria-label="Product added to shopping cart."]')).toBeVisible();
-    await expect(page.locator('[aria-label="Product added to shopping cart."]')).toContainText("Product added to shopping cart.");
-    await page.waitForTimeout(8000);
-    await expect(page.locator('[aria-label="Product added to shopping cart."]')).toBeHidden();
+    await cartLocator.addToCart();
+
+    const cartMessage = page.locator('[aria-label="Product added to shopping cart."]');
+    await expect(cartMessage).toBeVisible({timeout: 10000});
+    await expect(cartMessage).toContainText("Product added to shopping cart.");
+    await expect(cartMessage).toBeHidden({timeout: 10000});
     await expect(page.locator('[data-test="cart-quantity"]')).toContainText('1');
 
-    await page.locator(cartIconLocator).click();
+    await cartLocator.goToCart();
+   
     await expect(page).toHaveURL('https://practicesoftwaretesting.com/checkout');
     await expect(page.locator('[data-test="cart-quantity"]')).toContainText('1');
     await expect(page.locator('[data-test="product-title"]')).toContainText('Slip Joint Pliers ');
