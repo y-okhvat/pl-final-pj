263108defb0d1953ff59cb60daba89c89846e2a5
y-okhvat <lizaokhvat14031999@gmail.com>
2025-05-06 20:59:34 +0200

    fix: rewrote page object

--- pages/ProductPage.ts
+++ pages/ProductPage.ts
@@ -1,27 +1,55 @@
 import {Locator, Page} from "@playwright/test";
 
-export class productPage {
-    page: Page;
-    productLocator: Locator;
-    cartLocator:Locator;
-    cartIconLocator: Locator;
-    addToCartButton: Locator;
-    constructor (page: Page) {
+export class ProductPage {
+    readonly page: Page;
+    readonly firstProductNameInList: Locator;
+    readonly firstProductPriceInList: Locator;
+    readonly productNameOnPage: Locator;
+    readonly productPriceOnPage: Locator;
+    readonly cartIconLocator: Locator;
+    readonly addToCartButton: Locator;
+    readonly addToFavoritesButton: Locator;
+    readonly productNameList: Locator;
+     constructor (page: Page) {
         this.page = page;
-         this.productLocator = this.page.locator('text=Slip Joint Pliers');
-         this.cartLocator = this.page.locator('[data-test="add-to-cart"]');
-         this.cartIconLocator = this.page.locator('[data-test="nav-cart"]');
-         this.addToCartButton = this.page.locator('[data-test="add-to-cart"]')
+         this.firstProductNameInList = page.getByTestId("product-name").first();
+         this.firstProductPriceInList = page.getByTestId("product-price").first();
+         this.productNameOnPage = page.getByTestId("product-name");
+         this.productPriceOnPage = page.getByTestId("unit-price");
+         this.cartIconLocator = page.getByTestId("nav-cart");
+         this.addToCartButton = page.getByTestId("add-to-cart");
+         this.addToFavoritesButton = page.getByTestId("add-to-favorites");
+         this.productNameList = page.getByTestId("product-name");
 
     }
+    async openFirstProduct(): Promise<void> {
+        await this.firstProductNameInList.click();
+      }
 
-    async addToCart(): Promise<void> {
-        await this.addToCartButton.click(); 
-    
-}
-     async goToCart(): Promise<void> {
-         await this.cartIconLocator.click();
-}
+      async getNameFromList(): Promise<string> {
+        return await this.firstProductNameInList.innerText();
+      }
+
+      async getPriceFromList(): Promise<string> {
+        return await this.firstProductPriceInList.innerText();
+      }
+
+      async getNameFromProductPage(): Promise<string> {
+        return await this.productNameOnPage.innerText();
+      }
+
+      async getPriceFromProductPage(): Promise<string> {
+        return await this.productPriceOnPage.innerText();
+      }
+
+      async addToCart(): Promise<void> {
+        await this.addToCartButton.click();
+      }
+
+      async goToCart(): Promise<void> {
+        await this.cartIconLocator.click();
+      }
+
 }
 
 
