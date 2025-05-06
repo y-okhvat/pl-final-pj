dfcdbbcf0b4c6085639c37d50b1fd7dbdb6ed2c8
y-okhvat <lizaokhvat14031999@gmail.com>
2025-05-06 20:56:51 +0200

    fix: logic in product test

--- tests/2_product.spec.ts
+++ tests/2_product.spec.ts
@@ -1,16 +1,25 @@
 import { test, expect } from "@playwright/test";
-import {productPage} from '../pages/ProductPage';
+import {ProductPage} from '../pages/ProductPage';
 
-test('Login', async ({page}) => {
-    const productLocator = new productPage(page) ;
+test('Should display correct product info on product page', async ({page}) => {
+    const productPage = new ProductPage(page) ;
 
-await page.goto('https://practicesoftwaretesting.com/product/01JTG52YNVXSE95JS37D3QQ2Q6');
+await page.goto('https://practicesoftwaretesting.com');
 
 
-await expect(page).toHaveURL('https://practicesoftwaretesting.com/product/01JTG52YNVXSE95JS37D3QQ2Q6');
-await expect(page.locator('[data-test="product-name"]')).toContainText('Combination Pliers');
-await expect(page.locator('[data-test="unit-price"]')).toContainText('14.15');
-await expect(page.locator('[data-test="add-to-cart"]')).toBeVisible();
-await expect(page.locator('[data-test="add-to-favorites"]')).toBeVisible();
+const nameFromList = await productPage.getNameFromList();
+const priceFromList = await productPage.getPriceFromList();
 
+await productPage.openFirstProduct();
+
+const nameOnPage = await productPage.getNameFromProductPage();
+const priceOnPage = await productPage.getPriceFromProductPage();
+
+expect(nameOnPage).toBe(nameFromList);
+expect(Number(priceOnPage.replace('$', ''))).toBe(Number(priceFromList.replace('$', '')));
+
+await expect(productPage.addToCartButton).toBeVisible();
+await expect(productPage.addToFavoritesButton).toBeVisible();
 });
+
+
