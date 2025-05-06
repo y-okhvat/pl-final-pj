38ac3abba20e7b9e4a99f3d8b2553d6e222d2363
y-okhvat <lizaokhvat14031999@gmail.com>
2025-05-06 20:54:14 +0200

    Fix: Add array equality check with sorting to ensure comparison + fix locators

--- pages/home/home.page.ts
+++ pages/home/home.page.ts
@@ -10,14 +10,14 @@ readonly filters: FilterFragment;
 
 constructor(page:Page){
     this.page = page;
-    this.productName = this.page.locator('[data-test="product-name"]');
-    this.productPrice = this.page.locator('[data-test="product-price"]');
+    this.productName = page.getByTestId("product-name");
+    this.productPrice = page.getByTestId("product-price");
     this.filters = new FilterFragment(this.page);
 }
 async goto(): Promise<void> {
     await this.page.goto('/');
 }
-async getPtoductNames(): Promise<string[]> {
+async getProductNames(): Promise<string[]> {
     const productNames = await this.productName.allTextContents();
     return productNames;
 }
@@ -29,31 +29,27 @@ return productPrices.map((price) => parseInt(price.replace('$','').trim()));
 async expectSortedProducts (sortBy: SortOption): Promise<void> {
     switch (sortBy) {
         case 'Name (A - Z)': {
-            const productNames = await this.getPtoductNames();
-            const sortedProductNames = productNames.sort((a,b) => a.localeCompare(b) );
-            const areProductsSorted = productNames.join() === sortedProductNames.join();
-            expect(areProductsSorted,`Products are not sorted from ${sortBy}`).toBe(true);
+            const productNames = await this.getProductNames();
+            const sortedProductNames = [...productNames].sort((a, b) => a.localeCompare(b));
+            expect(productNames,`Products are not sorted from ${sortBy}`).toEqual(sortedProductNames);
             break;
         }
         case 'Name (Z - A)': {
-            const productNames = await this.getPtoductNames();
-            const sortedProductNames = productNames.sort((a,b) => b.localeCompare(a) );
-            const areProductsSorted = productNames.join() === sortedProductNames.join();
-            expect(areProductsSorted,`Products are not sorted from ${sortBy}`).toBe(true);  
+            const productNames = await this.getProductNames();
+            const sortedProductNames = [...productNames].sort((a, b) => b.localeCompare(a));
+            expect(productNames,`Products are not sorted from ${sortBy}`).toEqual(sortedProductNames);  
             break;
     }
     case 'Price (Low - High)': {
         const productPrices = await this.getProductPrices();
-        const sortedProductPrices = productPrices.sort((a,b) => a - b);
-        const areProductsSorted = productPrices.join() === sortedProductPrices.join();
-        expect(areProductsSorted,`Products are not sorted from ${sortBy}`).toBe(true);  
+        const sortedProductPrices = [...productPrices].sort((a, b) => a - b );
+        expect(productPrices,`Products are not sorted from ${sortBy}`).toEqual(sortedProductPrices);  
         break;
     }
     case 'Price (High - Low)': {
         const productPrices = await this.getProductPrices();
-        const sortedProductPrices = productPrices.sort((a,b) => b - a);
-        const areProductsSorted = productPrices.join() === sortedProductPrices.join();
-        expect(areProductsSorted,`Products are not sorted from ${sortBy}`).toBe(true);  
+        const sortedProductPrices = [...productPrices].sort((a, b) => b - a );
+        expect(productPrices,`Products are not sorted from ${sortBy}`).toEqual(sortedProductPrices);  
         break;
     }
     default:
