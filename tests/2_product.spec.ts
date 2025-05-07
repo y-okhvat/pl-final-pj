c83fe9ef60e216c432361bfd308591646d7c33bb
y-okhvat <lizaokhvat14031999@gmail.com>
2025-05-07 11:44:52 +0200

    add test steps and price helper to product test

--- tests/2_product.spec.ts
+++ tests/2_product.spec.ts
@@ -1,25 +1,37 @@
 import { test, expect } from "@playwright/test";
 import {ProductPage} from '../pages/ProductPage';
+import { parsePrice } from "../utils/priceHelper";
 
 test('Should display correct product info on product page', async ({page}) => {
     const productPage = new ProductPage(page) ;
+    let nameFromList: string;
+    let priceFromList:string;
 
-await page.goto('https://practicesoftwaretesting.com');
+await test.step('Navigate to the home page', async () => {
+    await page.goto('/');
+})
 
+await test.step('Get product name and price on home page ', async () => {
+nameFromList = await productPage.getFirstProductName();
+priceFromList = await productPage.getFirstProductPrice();
+});
 
-const nameFromList = await productPage.getNameFromList();
-const priceFromList = await productPage.getPriceFromList();
-
+await test.step('Open first product details page', async () => {
 await productPage.openFirstProduct();
+});
 
+await test.step('Verify product name and price match', async () => {
 const nameOnPage = await productPage.getNameFromProductPage();
 const priceOnPage = await productPage.getPriceFromProductPage();
 
 expect(nameOnPage).toBe(nameFromList);
-expect(Number(priceOnPage.replace('$', ''))).toBe(Number(priceFromList.replace('$', '')));
+expect(parsePrice(priceOnPage)).toBe(parsePrice(priceFromList));
 
+});
+
+await test.step('Verify Add to Cart and Add to Favorites buttons are visible', async () => {
 await expect(productPage.addToCartButton).toBeVisible();
 await expect(productPage.addToFavoritesButton).toBeVisible();
 });
 
-
+});
